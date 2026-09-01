7d54d997fcdffc73471d092de3ae256f01f60e37
MrgDev24 <programmerkg2400@gmail.com>
2026-09-02 01:41:42 +0800

    checkpoint

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
